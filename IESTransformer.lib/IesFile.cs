01ed1c665218246e5591abbf385c6c3ecda9aeb2
FinalCore <rimedheart@yandex.ru>
2020-02-25 21:10:44 +0300

    Перенес метод ExtractAnglesDim в класс IesFile

--- IESTransformer.lib/IesFile.cs
+++ IESTransformer.lib/IesFile.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace IESTransformer.lib
 {
@@ -23,6 +24,46 @@ namespace IESTransformer.lib
             {
                 iesFileContent.Add(iesFileRaw[i]);
             }
+        }
+
+        /// <summary>
+        /// Метод, извлекающий количество углов фотометрирования, заданных в ies файле.
+        /// </summary>
+        /// <param name="pattern"></param>
+        /// <returns></returns>
+        public void ExtractAnglesDim(out int alphaCount, out int bethaCount)
+        {
+            /* Извлекаем строку IES файла, в которой содержится информация о количестве углов, под которыми производились
+               измерения силы света */
+            string pattern = @"(\d{ 3,5}.\d{ 1,2})\s\d{ 1,3}.\d{ 1,3}";
+            Regex searchKey = new Regex(pattern);
+            for (int i = 0; i < iesFileContent.Count; i++)
+            {
+                if (searchKey.IsMatch(iesFileContent[i]))
+                {
+                    string anglesString = iesFileContent[i];
+                    break;
+                }
+            }
+            pattern = @"\s\d{1,3}\s\d{1,3}\s";
+            searchKey = new Regex(pattern);
+            string anglesDim = searchKey.Match(pattern).Value;
+            //Извлекаем численное значение углов из подстроки
+            char[] anglesDimChar = anglesDim.ToCharArray();
+            string anglesVert = "";
+            string anglesHor = "";
+            bool flagVH = true; // флаг, отвечающий за выбор вертикального или горизонтального угла
+            for (int i = 1; i < anglesDimChar.Length; i++)
+            {
+                if (Char.IsDigit(anglesDimChar[i])) // ошибка в этой строке (в anglesDimChar.Length - 2)
+                {
+                    if (flagVH) anglesVert += anglesDimChar[i];
+                    else anglesHor += anglesDimChar[i];
+                }
+                else flagVH = false;
+            }
+            alphaCount = int.Parse(anglesVert);
+            bethaCount = int.Parse(anglesHor);           
         }
     }
 }
